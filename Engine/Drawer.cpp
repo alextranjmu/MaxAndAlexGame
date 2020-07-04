d212abfc065b1614a8cebc4d147f20033ada10a3
Max <Max@MAX-PC>
2020-07-04 17:29:38 -0400

    complete

--- Engine/Drawer.cpp
+++ Engine/Drawer.cpp
@@ -45,7 +45,7 @@ void Drawer::DrawPPM_Horizontal_Flip(Graphics& gfx, int x, int y, PPM *frame)
 			}
 			else
 			{
-				gfx.PutPixel(col - x, y + row, p.red, p.green, p.blue);
+				gfx.PutPixel(x - col + frame->width, y + row, p.red, p.green, p.blue);
 			}
 		}
 	}
