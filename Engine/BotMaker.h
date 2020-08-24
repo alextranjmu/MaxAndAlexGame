1714c3690219d5f78777624d9cb49c7de2c7f289
Anh Alexander Tran <anh.alex.tran@gmail.com>
2020-08-23 23:25:51 -0400

    f

--- Engine/BotMaker.h
+++ Engine/BotMaker.h
@@ -8,10 +8,11 @@ class BotMaker : Obstacle
 {
 public:
 	BotMaker(int x, int y, int x1, int x2, int y1, int y2, std::string fileName, int rows, int columns);
+	int x;
+	int y;
 	queue<Enemy*> dead_slugs_gunbots;
 	queue<LazerBot*> dead_lazer_bots;
-	bool is_closed = true;
-	bool is_opening = false;
+	bool open = false;
 	void Draw(Graphics &gfx);
 
 };
