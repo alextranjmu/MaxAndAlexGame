8289bb02b98ce13ef90038921c386ef478052c81
Max <Max@MAX-PC>
2020-08-23 23:09:12 -0400

    sdf

--- Engine/Game.h
+++ Engine/Game.h
@@ -13,7 +13,7 @@
 #include "Bullet.h"
 #include "Enemy.h"
 #include "Point.h"
-#include "Vector.h" // dumbsas
+#include "Vector.h"
 #include "SpriteSheet.h"
 #include "Animation.h"
 #include "Wizard.h"
